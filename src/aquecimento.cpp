0392fc8d7f3b54381a1fd628c74d27ea96be3095
VinyPinheiro <viny-pinheiro@hotmail.com>
2015-10-11 22:22:22 -0300

    Implementando classe question na classe aquecimento

--- src/aquecimento.cpp
+++ src/aquecimento.cpp
@@ -14,75 +14,18 @@ Aquecimento::Aquecimento(const string& next)
     Environment *env = Environment::get_instance();
     set_dimensions(env->canvas->w(), env->canvas->h());
 
-    shared_ptr<Font> font =
-        env->resources_manager->get_font("res/fonts/AjarSans-Regular.ttf");
-    font->set_size(90);
-    font->set_style(Font::BOLD);
-    env->canvas->set_font(font);
-
     Image *image = new Image(this, "res/images/background_despolpa_fase2.png");
     add_child(image);
 
-    Text *title = new Text(this, "Despolpa", Color::BLACK);
+	vector<pair<string, string> > button;
 
-    if (title)
-    {
-        title->align_to(this, Object::CENTER, Object::NONE);
-        title->set_y(40);
-    }
-
-    add_child(title);
-
-    font->set_style(Font::NORMAL);
-    font->set_size(50);
-
-    Text *question = new Text(this, "Aquecer briquete moldado", Color::BLACK);
-
-    if (question)
-    {
-        question->align_to(this, Object::CENTER, Object::NONE);
-        question->set_y(title->y() + title->h() + 50);
-    }
-
+	button.emplace_back("Sim","aquecimentoAnimation");
+	button.emplace_back("Não","gameover");
+	
+    question = new Question(this, "Despolpa", "Aquecer briquete moldado para diminuir a umidade?"
+		, button);
     add_child(question);
-    
-    Text *question1 = new Text(this, "para diminuir a umidade?", Color::BLACK);
-
-    if (question1)
-    {
-        question1->align_to(this, Object::CENTER, Object::NONE);
-        question1->set_y(question->y() + question->h());
-    }
-
-    add_child(question1);
-
-    Button *yes = new Button(this, "aquecimentoAnimation", 200, 100);
-
-    if (yes)
-    {
-        yes->set_text("Sim", Color::BLACK);
-        yes->set_color(Color(20, 180, 55, 128), Color(20, 55, 128, 128));
-        yes->set_border(5, Color(0, 0, 0, 128));
-        yes->align_to(this, Object::CENTER, Object::NONE);
-        yes->set_y(question1->y() + question1->h() + 40);
-        yes->add_observer(this);
-    }
-
-    add_child(yes);
-
-    Button *no = new Button(this, "gameover", 200, 100);
-
-    if (no)
-    {
-        no->set_text("Não", Color::BLACK);
-        no->set_color(Color(20, 180, 55, 128), Color(20, 55, 128, 128));
-        no->set_border(5, Color(0, 0, 0, 128));
-        no->align_to(this, Object::CENTER, Object::NONE);
-        no->set_y(yes->y() + yes->h() + 20);
-        no->add_observer(this);
-    }
-
-    add_child(no);
+   
 }
 
 void
@@ -92,16 +35,13 @@ Aquecimento::draw_self()
     env->canvas->clear(Color::WHITE);
 }
 
-bool
-Aquecimento::on_message(Object *object, MessageID id, Parameters)
+
+void
+Aquecimento::update_self(unsigned long)
 {
-    if (id != Button::clickedID)
-    {
-        return false;
-    }
-
-    set_next(object->id());
-    finish();
-
-    return true;
+	if(question->finished())
+	{
+		this->set_next(question->next());
+		this->finish();
+	}
 }
